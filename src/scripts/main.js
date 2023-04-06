9f00ae063a5b90af20a49b3dd55bb25bcef91154
Andrey Lehendzevych <andrey.lehendzevych@gmail.com>
2023-04-06 12:05:32 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const largeImg = document.getElementById('largeImg');
+const thumbs = document.getElementById('thumbs');
+
+thumbs.addEventListener('click', (e) => {
+  const item = e.target.closest('.list-item__link');
+
+  if (!item || !thumbs.contains(item)) {
+    return;
+  }
+
+  e.preventDefault();
+  largeImg.src = item.href;
+});
